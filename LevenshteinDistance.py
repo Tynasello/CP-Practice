df617976f323eef795be58421e0df9c163dc688e
Tynasello <tynasello@gmail.com>
2021-03-07 11:27:39 -0500

    new problem

--- LevenshteinDistance.py
+++ LevenshteinDistance.py
@@ -5,29 +5,30 @@ you can replace, insert, or delete characters
 
 
 def solve(row, col):
-    res = 0
-    if col == 0 and row != 0:
-        res = row
-    elif row == 0 and col != 0:
-        res = col
-    if col == 0 or row == 0:
+    if row == 0 and col == 0:
         res = 0
+    elif row == 0:
+        res = col
+    elif col == 0:
+        res = row
+    elif dp[row][col] != -1:
+        res = dp[row][col]
     else:
         if s2[row - 1] == s1[col - 1]:
             res = solve(row - 1, col - 1)
         else:
             delet = solve(row, col - 1)
-            replace = solve(row - 1, col - 1)
             insert = solve(row - 1, col)
-            res = min(delet, replace, insert) + 1
+            replace = solve(row - 1, col - 1)
+            res = 1 + min(delet, replace, insert)
     dp[row][col] = res
     return res
 
 
-s1 = " pairs"
-s2 = " cars"
+s1 = "pairs"
+s2 = "cars"
 
 n1 = len(s1)
 n2 = len(s2)
-dp = [[0 for i in range(n1)] for i in range(n2)]
-print(solve(n2 - 1, n1 - 1))
+dp = [[-1 for i in range(n1 + 1)] for i in range(n2 + 1)]
+print(solve(n2, n1))
